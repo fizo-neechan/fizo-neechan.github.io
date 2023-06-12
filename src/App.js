e22b76e649ec50853fe9cdfd25e7d12f75a76d8f
fizo-neechan <Faizanknight5@gmail.com>
2023-06-12 05:22:23 +0500

    fixed non-local imports

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Spacer from './components/Spacer';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
